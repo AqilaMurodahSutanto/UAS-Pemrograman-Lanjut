3881d9c82e17525428fbe36142df1abcd4e368dd
Aqila Murodah Sutanto <143951779+AqilaMurodahSutanto@users.noreply.github.com>
2024-12-18 09:44:06 +0700

    Update sistembank.c

---
+++ sistembank.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Rekening
+{
+    
+}Nasabah;
+
+int main (){
+    int pilihan;
+
+    do
+    {
+        switch (pilihan)
+        {
+        case 1:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    } while (pilihan !=5);
+    
+}
